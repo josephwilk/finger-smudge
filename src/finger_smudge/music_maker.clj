5edceb4706228d852a0feb63e139b6ab26cfa617
Joseph Wilk <joe@josephwilk.net>
2016-06-25 21:46:30 +0100

    Load a sine wave in every note. Load the image and use the energy as the amp. Assume 1 pixel = 1 second... This is the bit that is deeply flawed but I'm curious anyway as to the output.

--- src/finger_smudge/music_maker.clj
+++ src/finger_smudge/music_maker.clj
@@ -2,6 +2,7 @@
   (:require [dynne.sound :as sound]
             [clojure.java.io :as io]
             [primitive-math :as p])
+  (:use [overtone.live])
   (:import [javax.sound.sampled
             AudioFileFormat$Type
             AudioFormat
@@ -28,6 +29,28 @@
     (-> (sound/sound duration (noise-fn))
         (sound/save  candidate 44100))
     candidate))
+
+
+(defsynth siney [note 0 amp 0 out-bus 0]
+  (let [freq (midicps note)
+        src (sin-osc freq)
+        src (* src amp)]
+    (out out-bus src)))
+
+(defonce synth-notes {:a  (siney (note :A3))
+                      :a# (siney (note :A#3))
+                      :b  (siney (note :B3))
+                      :c  (siney (note :C4))
+                      :c# (siney (note :C#4))
+                      :d  (siney (note :D4))
+                      :d# (siney (note :D#4))
+                      :e  (siney (note :E4))
+                      :f  (siney (note :F4))
+                      :f# (siney (note :F#4))
+                      :g  (siney (note :G4))
+                      :g# (siney (note :G#4))})
+
+(ctl (:a synth-notes) :amp 0.0)
 
 (comment
   (and-we-are-the-dreamer-of-the-dreams 120)
