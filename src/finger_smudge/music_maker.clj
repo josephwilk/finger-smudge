bb564431e8529a2915fddcd3ce3c67bc2e168e74
Joseph Wilk <joe@josephwilk.net>
2016-06-26 13:41:23 +0100

    Only use max energy.

--- src/finger_smudge/music_maker.clj
+++ src/finger_smudge/music_maker.clj
@@ -5,6 +5,7 @@
             [finger-smudge.fft :as fft]
             [mikera.image.core :as img]
             [mud.timing :as time]
+            [mud.core :as mud]
             ;;[clojure.core.matrix :as mat]
 
             [clojure.core.matrix :as mat]
@@ -40,8 +41,6 @@
     (-> (sound/sound duration (noise-fn))
         (sound/save  candidate 44100))
     candidate))
-
-
 
 (defsynth siney [note 0 amp-buf 0 out-bus 0  beat-bus (:count time/main-beat) beat-trg-bus (:beat time/main-beat)]
   (let [cnt      (in:kr beat-bus)
@@ -84,25 +83,61 @@
 (defn image->matrix [^BufferedImage bi rgb-to-val-fn]
   (let [h (.getHeight bi) ;;Notes is not rotated. lazy switch
         w (.getWidth bi)
-        M (mat/new-matrix w h)]
-    ;;(println (type M))
-    (dotimes [y w]
-      (dotimes [x h]
-        (let [v (rgb-to-val-fn (.getRGB bi y x))]
+        M (mat/new-matrix h w)]
+    (dotimes [row-idx h]
+      (dotimes [col-idx w]
+        (let [v (rgb-to-val-fn (.getRGB bi col-idx row-idx))]
           ;;(println y x)
-          (mat/mset! M y x v))))
+          (mat/mset! M row-idx col-idx v))))
     M))
 
 (mat/set-current-implementation :vectorz)
 
-(defonce fft-image    (img/load-image "image.png"))
-(def image-matrix (image->matrix fft-image fft/rgb-to-ngrey))
+(def fft-image    (img/load-image "resources/chroma_fft_image.png"))
+(def image-matrix  (mat/transpose (fft/file->image "image.data")))
+
+(def notes [:a :a# :b :c :c# :d :d# :e :f :f# :g :g#])
+
+(defn drop-nth
+  [n coll]
+  (->> coll
+       (map vector (iterate inc 1))
+       (remove #(zero? (mod (first %) n)))
+              (map second)))
+
+(defn write-max-energy-note-only! []
+  (doseq [[col-idx col] (map vector (range) (mat/columns image-matrix))]
+    (let [as  (into [] col)
+          max-idx (first (apply max-key second (map-indexed vector as)))
+          max-energy (nth as max-idx)
+          winning-note (nth notes max-idx)
+          lossing-notes (drop-nth max-idx notes)]
+
+      (doseq [[row-idx note] (map vector (range) notes)]
+        (if (= row-idx max-idx)
+          (do (println note max-energy) (buffer-write! (get amp-buffers note) col-idx [max-energy]))
+          (buffer-write! (get amp-buffers note) col-idx [0.0]))))))
+
+(defn write-energy! []
+  (doseq [[col-idx col] (map vector (range) (mat/columns image-matrix))]
+    (let [as  (into [] col)
+          max-idx (first (apply max-key second (map-indexed vector as)))
+          max-energy (nth as max-idx)
+          winning-note (nth notes max-idx)
+          lossing-notes (drop-nth max-idx notes)]
+
+      (doseq [[row-idx note] (map vector (range) notes)]
+        (if (= row-idx max-idx)
+          (do (println note max-energy) (buffer-write! (get amp-buffers note) col-idx [max-energy]))
+          (buffer-write! (get amp-buffers note) col-idx [0.0]))))))
+
 
 (comment
   (and-we-are-the-dreamer-of-the-dreams 120)
+  (mud/ctl-global-clock 6.0)
+  (write-max-energy-note-only!)
+  )
 
-  (doseq [[note idx-y row] (map vector [:a :a# :b :c :c# :d :d# :e :f :f# :g] (range) (mat/rows image-matrix))]
-    (doseq [[idx-x cell] (map vector (range) row)]
-      (let [cell (/ cell 2)]
-        (println cell)
-        (buffer-write! (get amp-buffers note) (* idx-y idx-x) [cell])))))
+(disj notes [:a])
+
+(max-key [0.2 0.3])
