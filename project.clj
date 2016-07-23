f7f64b59ffc0e6faaabeaab4d24dbe97ef8e27bc
Joseph Wilk <joe@josephwilk.net>
2016-07-23 20:08:31 +0100

    full mode for everything

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main finger-smudge.core
+
+  :global-vars {*print-length* false}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.18"]
                  [cheshire "5.6.1"]
